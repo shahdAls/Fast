d0dffe179a83213d3699349e52ef5f43b7c03898
shahdAls <128863844+shahdAls@users.noreply.github.com>
2024-02-23 02:18:27 +0300

    lets work!

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+
+def index():
+    return {'data':{'name':'sathak'}} # dectionary return json
+
+
+@app.get('/about')
+
+def about():
+    return {'data':'about page'}
+    
