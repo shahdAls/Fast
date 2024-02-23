625d0ae9d7f294ae92d4868b009d1c29679441e9
shahdAls <128863844+shahdAls@users.noreply.github.com>
2024-02-23 03:16:24 +0300

    up

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+# @app called decorator
+@app.get('/') #اسم app عادي اغيره
+# اسمه path
+def index(): # اسمها مش لازم اندكس
+    return {'data':'Blog list'} # dectionary return json
 
-@app.get('/')
 
-def index():
-    return {'data':{'name':'sathak'}} # dectionary return json
+# i move this before get id because it is dynamic and make proplem because the order is importent 
+@app.get('/blog/unpuplished')
 
-
-@app.get('/about')
-
-def about():
-    return {'data':'about page'}
+def unpuplished():
     
+    return {'data':'all unpuplished blog'}
+
+
+
+@app.get('/blog/{id}')
+#اسماء الفنكشن عادي تتكرر اذا كانت في روت مختلف 
+# كتبت {id} عشان تكون داينمك وتقرا من الريترن 
+
+def show(id:int):
+    # fetch blog with id=id
+    return {'data':id}
+
+
+
+@app.get('/blog/{id}/comments')    
+def comments(id):
+    # fetch blog with id=id
+    
+    return {'data':{'1','2'}}
